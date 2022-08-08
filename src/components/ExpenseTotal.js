60d48884ac1aecbc8a5777fead2e7982c78e6cd1
aneesh072 <aneeshkarki292@gmail.com>
2022-08-08 19:34:56 +1000

    expense banner added

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExpenseTotal = () => {
+  return (
+    <div className="alert alert-primary">
+      <span>Spend to far: $ 700</span>
+    </div>
+  );
+};
+
+export default ExpenseTotal;
