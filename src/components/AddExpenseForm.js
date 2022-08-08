cfe9b3fb23026ad6560db17830f31ab0b3fb42aa
aneesh072 <aneeshkarki292@gmail.com>
2022-08-08 21:23:05 +1000

    set states for the form values

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddExpenseForm = () => {
+  const [name, setName] = useState('');
+  const [cost, setCost] = useState('');
   return (
     <form>
       <div className="row"></div>
@@ -11,11 +13,19 @@ const AddExpenseForm = () => {
           type="text"
           className="form-control"
           id="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
         ></input>
       </div>
       <div className="col-sm">
         <label htmlFor="cost">Cost</label>
-        <input type="text" className="form-control" id="cost"></input>
+        <input
+          type="text"
+          className="form-control"
+          id="cost"
+          value={cost}
+          onChange={(event) => setCost(event.target.value)}
+        ></input>
       </div>
       <div className="col-sm">
         <button type="submit" className="btn btn-primary mt-2">
