beeb14b6b3e37160e05366eb6cb346da85ad5f80
aneesh072 <aneeshkarki292@gmail.com>
2022-08-08 20:57:23 +1000

    initialize reducer and app context

---
+++ src/context/AppContext.js
@@ -0,0 +1,26 @@
+import { createContext, useReducer } from 'react';
+
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  budget: 2000,
+  expenses: [
+    { id: 1, name: 'Shopping', cost: 40 },
+    { id: 2, name: 'Holday', cost: 10 },
+    { id: 3, name: 'Billing', cost: 20 },
+    { id: 4, name: 'Rent', cost: 402 },
+    { id: 5, name: 'Travel', cost: 407 },
+  ],
+};
+
+export const AppContext = createContext();
+
+//Provider
+const AppProvider = (props) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+};
