15a6ce92f60b4fa6b84caa3eed5ac017bf1418a9
aneesh072 <aneeshkarki292@gmail.com>
2022-08-08 22:28:38 +1000

    list deleting completed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
+  const { dispatch } = useContext(AppContext);
+
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: props.id,
+    });
+  };
+
   const { name, id, cost } = props;
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
       {props.name}
       <div>
-        <span className="badge rounded-pill text-bg-primary m-2">
-          £{cost}
-        </span>
-        <TiDelete size="1.5em" />
+        <span className="badge rounded-pill text-bg-primary m-2">${cost}</span>
+        <TiDelete
+          size="1.5em"
+          onClick={handleDeleteExpense}
+          className="position-relative"
+        />
       </div>
     </li>
   );
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const AppReducer = (state, action) => {
   switch (action.type) {
@@ -7,17 +8,26 @@ const AppReducer = (state, action) => {
         ...state,
         expenses: [...state.expenses, action.payload],
       };
+    case 'DELETE_EXPENSE':
+      return {
+        ...state,
+        expenses: [
+          state.expenses.filter((expense) => expense.id !== action.payload),
+        ],
+      };
     default:
       return state;
   }
 };
 
 const initialState = {
-  budget: 100000,
+  budget: 2002,
   expenses: [
-    { id: 1, name: 'Shopping', cost: 40 },
-    { id: 2, name: 'Holday', cost: 10 },
-    { id: 3, name: 'Billing', cost: 20 },
+    { id: uuidv4(), name: 'Shopping', cost: 50 },
+    { id: uuidv4(), name: 'Holiday', cost: 300 },
+    { id: uuidv4(), name: 'Transportation', cost: 70 },
+    { id: uuidv4(), name: 'Fuel', cost: 40 },
+    { id: uuidv4(), name: 'Child Care', cost: 500 },
   ],
 };
 
